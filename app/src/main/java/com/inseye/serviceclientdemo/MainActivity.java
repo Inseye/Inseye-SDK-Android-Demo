639aca064142c70ee0386dfec021728de9712cd6
Piotr Redmerski <piotr.redmerski@gmail.com>
2024-06-10 17:03:19 +0200

    added local repo

--- app/src/main/java/com/inseye/serviceclientdemo/MainActivity.java
+++ app/src/main/java/com/inseye/serviceclientdemo/MainActivity.java
@@ -24,34 +24,30 @@ import androidx.core.view.WindowCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.core.view.WindowInsetsControllerCompat;
 
-import com.inseye.shared.communication.ActionResult;
-import com.inseye.shared.communication.IBuiltInCalibrationCallback;
-import com.inseye.shared.communication.IEyetrackerEventListener;
-import com.inseye.shared.communication.IServiceBuiltInCalibrationCallback;
-import com.inseye.shared.communication.ISharedService;
-import com.inseye.shared.communication.IntActionResult;
-import com.inseye.shared.communication.TrackerAvailability;
+
+import com.inseye.sdk.GazeDataReader;
+import com.inseye.sdk.InseyeSDK;
+import com.inseye.sdk.InseyeTracker;
+import com.inseye.sdk.InseyeTrackerException;
 
 import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GazeDataReader.IGazeData, InseyeTracker.IEyeTrackerStatusListener {
     private final String TAG = MainActivity.class.toString();
-    private ISharedService inseyeServiceClient;
-    private InseyeServiceBinder inseyeServiceBinder;
-    private GazeDataReader gazeDataReader;
-
-    //view
     private TextView statusTextView, gazeDataTextView;
     private Button calibrateButton, subGazeDataButton, unsubGazeDataButton;
     private RedPointView redPointView;
+
     private final Handler mainLooperHandler = new Handler(Looper.getMainLooper());
 
+    private InseyeSDK inseyeSDK;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
@@ -60,180 +56,78 @@ public class MainActivity extends AppCompatActivity {
                 View.SYSTEM_UI_FLAG_HIDE_NAVIGATION|
                         View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
 
+
         setContentView(R.layout.activity_main);
-
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
         calibrateButton = findViewById(R.id.calibButton);
         subGazeDataButton = findViewById(R.id.subscribeGazeButton);
         unsubGazeDataButton = findViewById(R.id.unsubscribeGazeButton);
         statusTextView = findViewById(R.id.textViewStatus);
         gazeDataTextView = findViewById(R.id.textViewGazeData);
-        calibrateButton.setOnClickListener( view -> RunCalibration());
-        subGazeDataButton.setOnClickListener( view -> SubscribeGazeData());
-        unsubGazeDataButton.setOnClickListener( view -> UnsubscribeGazeData());
-
         redPointView = findViewById(R.id.redPointView);
 
-        inseyeServiceBinder = new InseyeServiceBinder(this);
-        inseyeServiceBinder.bind(new InseyeServiceBinder.IServiceBindCallback() {
-            @Override
-            public void serviceConnected(ISharedService service) {
-                inseyeServiceClient = service;
-                Toast.makeText(MainActivity.this, "inseye service connected", Toast.LENGTH_SHORT).show();
+        inseyeSDK = new InseyeSDK(this);
 
-                //get current tracker status
-                try {
-                    statusTextView.setText(inseyeServiceClient.getTrackerAvailability().toString());
-                    if(inseyeServiceClient.getTrackerAvailability() == TrackerAvailability.Available)
-                        SubscribeGazeData();
+//        InseyeTracker eyeTracker = null;
+//        try {
+//            eyeTracker = inseyeSDK.getEyeTracker().get();
+//        } catch (ExecutionException | InterruptedException e) {
+//            Log.e(TAG, e.getMessage());
+//            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+//        }
 
-                } catch (RemoteException e) {
-                    throw new RuntimeException(e);
-                }
+//        if(eyeTracker != null && inseyeSDK.isServiceConnected()) {
 
-                //subscribe to tracker status event
+        inseyeSDK.getEyeTracker().thenAccept(insEyeTracker -> {
+            statusTextView.setText(insEyeTracker.getTrackerAvailability());
 
-                try {
-                    inseyeServiceClient.subscribeToEyetrackerEvents(new IEyetrackerEventListener.Stub() {
-                        @Override
-                        public void handleTrackerAvailabilityChanged(TrackerAvailability availability) {
-                            mainLooperHandler.post(() -> statusTextView.setText(availability.toString()));
-                        }
-                    });
-                } catch (RemoteException e) {
-                    throw new RuntimeException(e);
-                }
+            insEyeTracker.subscribeToTrackerStatus(this);
 
-            }
-
-            @Override
-            public void serviceDisconnected() {
-                Toast.makeText(MainActivity.this, "inseye service disconnected", Toast.LENGTH_SHORT).show();
-                inseyeServiceClient = null;
-            }
-
-            @Override
-            public void serviceError(Exception e) {
-                Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
-
-    private void UnsubscribeGazeData() {
-        if(!inseyeServiceBinder.isConnected()) {
-            Toast.makeText(this, "not bound to service", Toast.LENGTH_SHORT).show();
-            return;
-        }
-
-        try {
-            inseyeServiceClient.stopStreamingGazeData();
-            if(gazeDataReader != null) gazeDataReader.close();
-        } catch (RemoteException e) {
-            Log.e(TAG, e.toString());
-        }
-    }
-
-
-    private void SubscribeGazeData() {
-        if(!inseyeServiceBinder.isConnected()) {
-            Toast.makeText(this, "not bound to service", Toast.LENGTH_SHORT).show();
-            return;
-        }
-
-        try {
-            IntActionResult result = inseyeServiceClient.startStreamingGazeData();
-            if(result.success) {
-                int udpPort = result.value;
-                Log.i(TAG, "port:" + udpPort);
-                gazeDataReader = new GazeDataReader(udpPort, gazeData -> {
-                        mainLooperHandler.post(() -> gazeDataTextView.setText(String.format(Locale.US, "Left Eye:   X:%6.2f Y:%6.2f\nRight Eye: X:%6.2f Y:%6.2f\nEvent: %s\nTime: %d",
-                                gazeData.left_x, gazeData.left_y, gazeData.right_x, gazeData.right_y, gazeData.event, gazeData.timeMilli)));
-
-                    float avgGazeX = (gazeData.left_x + gazeData.right_x) / 2f;
-                    float avgGazeY = (gazeData.left_y + gazeData.right_y) / 2f;
-                    // gaze data in radians where (0,0) is in screen center
-                    redPointView.post(()-> redPointView.setPoint(avgGazeX, avgGazeY));
+            calibrateButton.setOnClickListener(v -> {
+                insEyeTracker.startCalibration().thenAccept(result -> {
+                    Toast.makeText(this, result.toString(), Toast.LENGTH_SHORT).show();
                 });
-                gazeDataReader.start();
-            } else {
-                Log.e(TAG, "gaze stream error: " + result.errorMessage);
-                Toast.makeText(this, "gaze stream error: " + result.errorMessage, Toast.LENGTH_SHORT).show();
-            }
+            });
 
-        } catch (RemoteException | SocketException | UnknownHostException e) {
-            Log.e(TAG, e.toString());
-            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    private void RunCalibration() {
-        if(!inseyeServiceBinder.isConnected()) {
-            Toast.makeText(this, "not bound to service", Toast.LENGTH_SHORT).show();
-            return;
-        }
-
-        ActionResult result = new ActionResult();
-        try {
-            IServiceBuiltInCalibrationCallback calibrationAbortHandler = inseyeServiceClient.startBuiltInCalibrationProcedure(result, new IBuiltInCalibrationCallback.Stub() {
-                @Override
-                public void finishCalibration(ActionResult calibrationResult) throws RemoteException {
-                    Log.i(TAG, calibrationResult.toString());
-                    if(calibrationResult.successful) {
-                        Log.e(TAG, "calibration success");
-                        Toast.makeText(MainActivity.this, "calibration success", Toast.LENGTH_SHORT).show();
-                    }
-                    else {
-                        Log.e(TAG, "calibration fail: " + calibrationResult.errorMessage);
-                        Toast.makeText(MainActivity.this, "calibration fail: " + calibrationResult.errorMessage, Toast.LENGTH_SHORT).show();
-                    }
+            subGazeDataButton.setOnClickListener(v -> {
+                try {
+                    insEyeTracker.subscribeToGazeData(this);
+                } catch (InseyeTrackerException e) {
+                    Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
                 }
             });
-        } catch (RemoteException e) {
-            Log.e(TAG, "calibration remote exception: " + e);
-            return;
-        }
-        if(result.successful) {
-            Log.i(TAG, "calibration init success");
-            Toast.makeText(MainActivity.this, "calibration init success", Toast.LENGTH_SHORT).show();
-            //calibrationAbortHandler could be used to abort calibration
-        }
-        else {
-            Log.e(TAG, "calibration init fail: " + result.errorMessage);
-            Toast.makeText(this, "calibration init fail: " + result.errorMessage, Toast.LENGTH_SHORT).show();
-            //calibrationAbortHandler is null
-        }
-    }
 
-    @Override
-    protected void onResume() {
-        if(inseyeServiceBinder.isConnected()) {
-            try {
-                statusTextView.setText(inseyeServiceClient.getTrackerAvailability().toString());
-            } catch (RemoteException e) {
-                throw new RuntimeException(e);
-            }
-        }
-        super.onResume();
-    }
+            unsubGazeDataButton.setOnClickListener(v -> insEyeTracker.unsubscribeFromGazeData());
 
-    @Override
-    protected void onStop() {
-        super.onStop();
+        }).exceptionally(throwable -> {
+            Toast.makeText(this, throwable.getMessage(), Toast.LENGTH_SHORT).show();
+            Log.e(TAG, throwable.getMessage());
+            return null;
+        });
     }
 
     @Override
     protected void onDestroy() {
-        try {
-            if(inseyeServiceBinder.isConnected()) {
-                inseyeServiceClient.unsubscribeFromEyetrackerEvents();
-                if(gazeDataReader != null)
-                    gazeDataReader.close();
-            }
-        } catch (RemoteException e) {
-            throw new RuntimeException(e);
-        }
-        inseyeServiceBinder.unbind();
+        inseyeSDK.disposeEyeTracker();
         super.onDestroy();
+    }
+
+    @Override
+    public void nextGazeDataReady(GazeData gazeData) {
+        mainLooperHandler.post(() -> gazeDataTextView.setText(String.format("Left Eye:   X:%6.2f Y:%6.2f\nRight Eye: X:%6.2f Y:%6.2f\nEvent: %s\nTime: %d",
+                gazeData.left_x, gazeData.left_y, gazeData.right_x, gazeData.right_y, gazeData.event, gazeData.timeMilli)));
+        float avgGazeX = (gazeData.left_x + gazeData.right_x) / 2f;
+        float avgGazeY = (gazeData.left_y + gazeData.right_y) / 2f;
+        // gaze data in radians where (0,0) is in screen center
+        redPointView.post(()-> redPointView.setPoint(avgGazeX, avgGazeY));
+    }
+
+    @Override
+    public void onTrackerAvailabilityChanged(TrackerAvailability availability) {
+        mainLooperHandler.post(() -> statusTextView.setText(availability.name()));
     }
 }
